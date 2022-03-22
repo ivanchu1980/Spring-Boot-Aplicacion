fedf61e3627e654d6c56391e67f9be345a5cc8cc
Ivanchu <ivanchu1980@hotmail.com>
2022-03-22 23:46:06 +0100

    Commit inicial con eclipse

---
+++ aplicacion/src/main/java/com/ivan/app/MicroserviciosApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviciosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviciosApplication.class, args);
+	}
+
+}
---
+++ aplicacion/src/test/java/com/ivan/app/MicroserviciosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviciosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
